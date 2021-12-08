92b326d63bde4818f1b1c9c1fc30da20d448c384
molaru <molaru@genetec.com>
2021-12-08 09:51:46 -0500

    SherlockAndAnagrams

---
+++ src/SherlockAndAnagrams/SherlockAndAnagrams.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SherlockAndAnagrams
+{
+    using System.Diagnostics;
+
+    public class SherlockAndAnagrams
+    {
+        public static int Calculate(string s)
+        {
+            var result = 0;
+            var collection = new Dictionary<string,int>();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var substring = s[i].ToString();
+
+                if (collection.ContainsKey(substring))
+                {
+                    collection[substring]++; 
+                    result += collection[substring];
+                    Debug.WriteLine(substring);
+                }
+                else
+                {
+                    collection.Add(substring,0);
+                }
+                
+                for (var j = i+1; j < s.Length; j++)
+                {
+                    substring += s[j];
+                    var sortedString = string.Join("",substring.OrderBy(x => x));
+                    if (collection.ContainsKey(sortedString))
+                    {
+                        collection[sortedString]++;
+                        result+= collection[sortedString];
+                        Debug.WriteLine(substring);
+                    }
+                    else
+                    {
+                        collection.Add(sortedString, 0);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ tests/FibonacciModifiedTests/SherlockAndAnagramsTests/SherlockAndAnagramsTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace SherlockAndAnagramsTests
+{
+    public class SherlockAndAnagramsTests
+    {
+        [Theory]
+        [InlineData("abba", 4)]
+        [InlineData("abcd", 0)]
+        [InlineData("ifailuhkqq", 3)]
+        [InlineData("cdcd", 5)]
+        [InlineData("kkkk", 10)]
+        public void TestCases(string s,int expected)
+        {
+            var result = SherlockAndAnagrams.SherlockAndAnagrams.Calculate(s);
+
+            Assert.Equal(expected,result);
+        }
+    }
+}
