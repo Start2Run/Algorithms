b14d059a35eeff894bc522d59998d274903ea092
molaru <molaru@genetec.com>
2022-02-07 14:50:16 -0500

    FraudulentActivityNotifications

---
+++ src/Sorting/FraudulentActivityNotifications.cs
@@ -0,0 +1,34 @@
+﻿// ==========================================================================
+// Copyright (C) 2022 by Genetec, Inc.
+// All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+// ==========================================================================
+
+namespace Sorting
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class FraudulentActivityNotifications
+    {
+        public static int ActivityNotifications(List<int> expenditure, int d)
+        {
+            var result = 0;
+
+            var middle = d / 2;
+            for (var i=d;i<expenditure.Count;i++)
+            {
+                var temp = expenditure.GetRange(i - d,d).OrderBy(x => x).ToArray();
+
+                var median = (d % 2 == 0)?temp[middle - 1] + temp[middle]: temp[middle];
+
+                if (median * 2 <= expenditure[i])
+                {
+                    result++;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ tests/SortingComparatorTests/FraudulentActivityNotificationsTests.cs
@@ -0,0 +1,26 @@
+﻿// ==========================================================================
+// Copyright (C) 2022 by Genetec, Inc.
+// All rights reserved.
+// May be used only in accordance with a valid Source Code License Agreement.
+// ==========================================================================
+
+namespace SortingTests
+{
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Xunit;
+    using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+
+    [TestClass]
+    public class FraudulentActivityNotificationsTests
+    {
+
+        [Theory]
+        [InlineData(new int[] { 2, 3, 4, 2, 3, 6, 8, 4, 5 }, 5, 2)]
+        public void TestMethod(int[] data, int d, int expected)
+        {
+            var result = Sorting.FraudulentActivityNotifications.ActivityNotifications(data.ToList(), d);
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
